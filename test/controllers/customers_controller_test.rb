3c9646015734fe16378c817cbf854ed6054ca32b
Paige Davenport <davenport.paige@outlook.com>
2019-11-05 15:51:32 -0800

    Added index tests for Customers controller

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -9,6 +9,29 @@ describe CustomersController do
       must_respond_with :ok
     end
 
+    it "responds with an array of customer hashes" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+      body.each do |customer|
+        expect(customer).must_be_instance_of Hash
+        expect(customer.keys.sort).must_equal ["id", "movies_checked_out_count", "name", "phone", "postal_code", "registered_at"]
+      end
+    end
+
+    it "will respond with an empty array when there are no customers" do
+      Rental.destroy_all
+      Customer.destroy_all
+  
+      get customers_path
+      body = JSON.parse(response.body)
+  
+      expect(body).must_be_instance_of Array
+      expect(body).must_equal []
+    end
+
   end
 
 
