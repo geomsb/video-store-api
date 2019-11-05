92abc7c7ac64baa5d010c34f57e3bb085d064695
Paige Davenport <davenport.paige@outlook.com>
2019-11-05 14:40:18 -0800

    Added relationship to Rental and updated test_helper

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 describe Customer do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
+  # describe "relationships" do
+  #   it "can have many rentals" do
+  #     customer = Customer.first
+  #     movie = Movie.first
+
+  #     rental = Rental.create(movie: movie, customer: customer)
+
+  #     expect(customer.rentals.count).must_be :>, 0
+  #     customer.rentals.each do |rental|
+  #       expect(rental).must_be_instance_of Rental
+  #     end
+  #   end
   # end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'minitest/rails'
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
