63666ab812c92fff68e632760847aa51a28ed0d0
Paige Davenport <davenport.paige@outlook.com>
2019-11-05 15:04:11 -0800

    Added rental fixtures and relationship test

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 describe Rental do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relationships" do
+    before do
+      @customer = customers(:betsy)
+      @movie = movies(:new_movie)
+    end
+
+    it "can set the customer through 'customer'" do
+      rental = Rental.new(movie_id: @movie.id)
+      rental.customer = @customer
+
+      expect(rental.customer_id).must_equal @customer.id
+    end
+
+    it "can set the customer through 'customer_id'" do
+      rental = Rental.new(movie_id: @movie.id)
+      rental.customer_id = @customer.id
+
+      expect(rental.customer).must_equal @customer
+
+    end
+
+    it "can set the movie through 'movie'" do
+      rental = Rental.new(customer_id: @customer.id)
+      rental.movie = @movie
+
+      expect(rental.movie_id).must_equal @movie.id
+    end
+
+    it "can set the movie through 'movie_id'" do
+      rental = Rental.new(customer_id: @customer.id)
+      rental.movie_id = @movie.id
+
+      expect(rental.movie).must_equal @movie
+    end
+
+  end
 end
