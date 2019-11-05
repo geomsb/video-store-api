0e4c688e782396428be3d38f97757f7c5dc9e683
Paige Davenport <davenport.paige@outlook.com>
2019-11-05 13:27:36 -0800

    Added registered_at column to customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_210201) do
+ActiveRecord::Schema.define(version: 2019_11_05_212543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2019_11_05_210201) do
     t.string "state"
     t.string "postal_code"
     t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "registered_at"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "overview"
+    t.date "realease_date"
+    t.integer "inventory"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
