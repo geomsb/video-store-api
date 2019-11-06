d028691236f13be8bfef3460bc291badb7f7c8f9
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-11-05 16:00:22 -0800

    index controller added to movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,6 @@
 class MoviesController < ApplicationController
+  def index
+    movies = Movie.all
+    render json: movies.as_json(only: [:id, :release_date, :title]), status: :ok
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do
+    it "responds with JSON and success" do
+      get movies_path
+
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :ok
+    end
+
+    it "responds with an array of movies hashes" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+      body.each do |movie|
+        expect(movie).must_be_instance_of Hash
+        expect(movie.keys.sort).must_equal ["id", "release_date", "title"]
+      end
+    end
+
+    it "will respond with an empty array when there are no movies" do
+      Rental.destroy_all
+      Movie.destroy_all
+  
+      get movies_path
+      body = JSON.parse(response.body)
+  
+      expect(body).must_be_instance_of Array
+      expect(body).must_equal []
+    end
+  end
 end
