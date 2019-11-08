0e3241516d73e2cba038eea1ee21d78b53e3640a
Paige Davenport <davenport.paige@outlook.com>
2019-11-07 20:50:20 -0800

    Added custom method tests for Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,13 @@
 class Movie < ApplicationRecord
+  after_create :set_avail_inv
+
   has_many :rentals
 
-  validates :title, :overview, :release_date, presence: true
-  validates :inventory, numericality: true, presence: true
-
+  validates :title, :overview, :release_date, :inventory, presence: true
+  validates_numericality_of :inventory, greater_than: 0
+  
   def movie_avail?
-    if self.available_inventory > 1
+    if self.available_inventory >= 1
       self.reduce_avail_inv
       return true
     else
@@ -21,5 +23,9 @@ class Movie < ApplicationRecord
   def check_in
     self.available_inventory = self.available_inventory + 1
     self.save
+  end
+
+  def set_avail_inv
+    self.available_inventory ||= self.inventory
   end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -14,13 +14,22 @@ describe Movie do
 
     it "will have the required fields" do
       @movies.each do |movie|
-        [:title, :overview, :release_date].each do |field|
-          expect(item).must_respond_to field
+        [:title, :overview, :release_date, :inventory].each do |field|
+          expect(movie).must_respond_to field
         end
       end
-
     end
 
+    it "must have an inventory greater than 0" do
+      movie = Movie.new(
+        title: "A title",
+        overview: "An interesting film",
+        release_date: "2019-10-03",
+        inventory: 0
+      )
+
+      refute(movie.valid?)
+    end
   end
 
   describe "relationships" do
@@ -35,6 +44,86 @@ describe Movie do
       expect(@movie.rentals.count).must_be :>, 0
       @movie.rentals.each do |rental|
         expect(rental).must_be_instance_of Rental
+      end
+    end
+  end
+
+  describe "custom methods" do
+    describe "movie_avail?" do
+      it "returns true if available inventory is greater than 0 and decreases available inventory by 1" do
+        movie = Movie.first
+        available = movie.available_inventory
+
+
+        result = movie.movie_avail?
+
+        expect(result).must_equal true
+        expect(movie.available_inventory).must_equal (available - 1)
+      end
+
+      it "returns false if available inventory is 0" do
+        movie = Movie.last
+        movie.update(available_inventory: 0)
+
+        result = movie.movie_avail?
+
+        expect(result).must_equal false
+        expect(movie.available_inventory).must_equal 0
+      end
+    end
+
+    describe "reduce_avail_inv" do
+      it "reduces available inventory by 1" do
+        movie = Movie.last
+        available = movie.available_inventory
+
+        movie.reduce_avail_inv
+
+        expect(movie.available_inventory).must_equal (available - 1)
+      end
+
+      #edge case?
+    end
+
+    #TODO
+    # describe "check_in" do
+    #   it "" do
+    #   end
+    # end
+
+    describe "set_avail_inv" do
+      it "sets available inventory to inventory when a new movie is created" do
+        movie_1 = Movie.create(
+          title: "Pan's Labyrinth",
+          overview: "An interesting film",
+          release_date: "2009-10-03",
+          inventory: 2
+        )
+
+        movie_2 = Movie.new(
+          title: "Pitch Perfect",
+          overview: "A movie about acapella",
+          release_date: "2009-10-10",
+          inventory: 3
+        )
+        movie_2.save
+
+        expect(movie_1.available_inventory).must_equal movie_1.inventory
+        expect(movie_2.available_inventory).must_equal movie_2.inventory
+      end
+
+      it "does not change available inventory when a movie is updated" do
+        movie_1 = Movie.create(
+          title: "Pan's Labyrinth",
+          overview: "An interesting film",
+          release_date: "2009-10-03",
+          inventory: 2
+        )
+
+        movie_1.reduce_avail_inv
+        movie_1.update(release_date: "2006-12-29")
+
+        expect(movie_1.available_inventory).must_equal 1
       end
     end
   end
